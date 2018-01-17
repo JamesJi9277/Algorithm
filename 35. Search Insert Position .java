838f3c89979e5f2c862de97c761c5ec898dc46ac
JamesJi <jiqi@gwu.edu>
2018-01-16 21:39:41 -0800

    happy Tuesday

---
+++ 35. Search Insert Position .java
@@ -0,0 +1,27 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int start = 0;
+        int end = nums.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+        // 到最后只剩下两个数但是并不排除相等的可能性
+        if (nums[start] >= target) {
+            return start;
+        } else if (nums[end] >= target) {
+            return end;
+        } else {
+            return end + 1;
+        }
+    }
+}
