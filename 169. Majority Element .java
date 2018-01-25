638f3dbdec6d4367febf4e019d2f92fc7ba752f1
JamesJi <jiqi@gwu.edu>
2018-01-24 21:33:04 -0800

    good night

---
+++ 169. Majority Element .java
@@ -0,0 +1,42 @@
+// 时间On
+// 空间On
+class Solution {
+    public int majorityElement(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int length = nums.length;
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i : nums) {
+            int count = map.getOrDefault(i, 0);
+            if (count + 1 > length / 2) {
+                return i;
+            } else {
+                map.put(i, count + 1);
+            }
+        }
+        return 0;
+    }
+}
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int count = 0;
+        int result = nums[0];
+        for (int i = 0; i < nums.length; i++) {
+            if (result == nums[i]) {
+                count++;
+            } else {
+                count--;
+            }
+            if (count == 0) {
+                result = nums[i];
+                count = 1;
+            }
+        }
+        return result;
+    }
+}
