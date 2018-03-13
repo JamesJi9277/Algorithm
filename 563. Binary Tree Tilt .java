7952b7c4edfbfc3a6868f0f98a3fde60bd50d11c
JamesJi <jiqi@gwu.edu>
2018-03-12 22:08:19 -0700

    happy Monday

---
+++ 563. Binary Tree Tilt .java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int findTilt(TreeNode root) {
+        int[] res = new int[1];
+        helper(root, res);
+        return res[0];
+    }
+    private int helper(TreeNode root, int[] res) {
+        if (root == null) {
+            return 0;
+        }
+        int left = helper(root.left, res);
+        int right = helper(root.right, res);
+        res[0] += Math.abs(left - right);
+        return left + right + root.val;
+    }
+}
--- 70. Climbing Stairs .java
+++ 70. Climbing Stairs .java
@@ -17,3 +17,17 @@ class Solution {
         return res[n];
     }
 }
+
+
+class Solution {
+    public int climbStairs(int n) {
+        int[] res = new int[n + 2];
+        res[0] = 1;
+        res[1] = 1;
+        res[2] = 2;
+        for (int i = 3; i < n + 2; i++) {
+            res[i] = res[i - 1] + res[i - 2];
+        }
+        return res[n];
+    }
+}
