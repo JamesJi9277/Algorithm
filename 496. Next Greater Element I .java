2fe39d252db0fd71740d078d93658ea604f3721b
JamesJi <jiqi@gwu.edu>
2018-03-25 19:12:39 -0700

    end of today

---
+++ 496. Next Greater Element I .java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        Stack<Integer> stack = new Stack<Integer>();
+        for (int i : nums2) {
+            while (!stack.isEmpty() && stack.peek() < i) {
+                // map里面key是actual value，value是next greater value
+                map.put(stack.pop(), i);
+            }
+            stack.push(i);
+        }
+        for (int i = 0; i < nums1.length; i++) {
+            nums1[i] = map.getOrDefault(nums1[i], -1);
+        }
+        return nums1;
+    }
+}
