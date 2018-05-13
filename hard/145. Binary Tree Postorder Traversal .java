7fa51553927f62e6332f6417b45ea26329d8ca53
JamesJi <jiqi@gwu.edu>
2018-05-13 12:18:05 -0700

    remember postorder iterative solution

--- hard/145. Binary Tree Postorder Traversal .java
+++ hard/145. Binary Tree Postorder Traversal .java
@@ -25,3 +25,34 @@ class Solution {
         res.add(root.val);
     }
 }
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        if (root == null) {
+            return res;
+        }
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode temp = stack.pop();
+            res.add(0, temp.val);
+            if (temp.left != null) {
+                stack.push(temp.left);
+            }
+            if (temp.right != null) {
+                stack.push(temp.right);
+            }
+        }
+        return res;
+    }
+}
