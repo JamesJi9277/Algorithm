95ba4ffd507acf700c7a52d2b4dc3b5b8ca43398
JamesJi <jiqi@gwu.edu>
2018-05-15 21:50:16 -0700

    keep fighting Tuesday

---
+++ medium/451. Sort Characters By Frequency .java
@@ -0,0 +1,41 @@
+class Solution {
+    public String frequencySort(String s) {
+        if (s == null || s.length() == 0) {
+            return "";
+        }
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+        }
+        List<Pair> list = new ArrayList<>();
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+            list.add(new Pair(entry.getKey(), entry.getValue()));
+        }
+        list.sort(null);
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < list.size(); i++){
+            int count = list.get(i).value;
+            char c = list.get(i).character;
+            
+            while(count > 0){
+                sb.append(c);
+                count--;
+            }
+        }
+        
+        return sb.toString();
+    }
+}
+class Pair implements Comparable<Pair> {
+    Character character;
+    int value;
+    public Pair() {};
+    public Pair(Character c, int value) {
+        this.character = c;
+        this.value = value;
+    }
+    @Override
+    public int compareTo(Pair that) {
+        return that.value - this.value;
+    }
+}
--- medium/808. Soup Servings .java
+++ medium/808. Soup Servings .java
@@ -5,7 +5,7 @@ class Solution {
         }
         return helper(n, n, new Double[n + 1][n + 1]);
     }
-    private int helper(int a, int b, Double[][] memo) {
+    private double helper(int a, int b, Double[][] memo) {
         if (a <= 0 && b <= 0) {
             return 0.5;
         }
@@ -19,11 +19,11 @@ class Solution {
             return memo[a][b];
         }
         int[] serveA = {100, 75, 50, 25};
-        int[] serveB = {0, 25, 50, 100};
-        memp[a][b] = 0.0;
+        int[] serveB = {0, 25, 50, 75};
+        memo[a][b] = 0.0;
         for (int i = 0; i < 4; i++) {
             memo[a][b] += helper(a - serveA[i], b - serveB[i], memo);
         }
-        return memo[a][b] * 0.25;
+        return memo[a][b] *= 0.25;
     }
 }
