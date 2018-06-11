bece4b496c6c04eedba2d3098b7e15496fc98a34
JamesJi <jiqi@gwu.edu>
2018-06-10 21:11:55 -0700

    one more and good night

--- medium/518. Coin Change 2 .java
+++ medium/518. Coin Change 2 .java
@@ -24,3 +24,16 @@ class Solution {
         }
     }
 }
+
+class Solution {
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int i = 0; i < coins.length; i++) {
+            for (int j = 0; j < amount && j + coins[i] <= amount; j++) {
+                dp[j + coins[i]] += dp[j];
+            }
+        }
+        return dp[amount];
+    }
+}
