9be53439b20be0e900acbf59b4e90b593c0ddddb
JamesJi <jiqi@gwu.edu>
2018-06-13 19:49:23 -0700

    good staret

---
+++ medium/422. Valid Word Square .java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean validWordSquare(List<String> words) {
+        int ct = 0;
+        for (String s : words) {
+            String colRead = columnRead(words, ct);
+            ct++;
+            if (!colRead.equals(s)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    private String columnRead(List<String> words, int index) {
+        StringBuilder sb = new StringBuilder();
+        for (String s : words) {
+            if (index < s.length()) {
+                sb.append(s.charAt(index));
+            }
+        }
+        return sb.toString();
+    }
+}
