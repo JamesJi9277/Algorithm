b8ffaa17f06f0abfda9b0007a407b113b60be41c
JamesJi <jiqi@gwu.edu>
2018-06-13 20:55:12 -0700

    good start and clam down

--- medium/22. Generate Parentheses .java
+++ medium/22. Generate Parentheses .java
@@ -26,3 +26,30 @@ class Solution {
         }
     }
 }
+
+// second
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<String>();
+        if (n < 0) {
+            return res;
+        }
+        helper(n, n, res, "");
+        return res;
+    }
+    private void helper(int left, int right, List<String> res, String s) {
+        if (left > right) {
+            return;
+        }
+        if (left == 0 && right == 0) {
+            res.add(new String(s));
+            return;
+        }
+        if (left > 0) {
+            helper(left - 1, right, res, s + "(");
+        }
+        if (right > 0) {
+            helper(left, right - 1, res, s + ")");
+        }
+    }
+}
