597f8cac0d1d8c8a94e6de08dfbbba87ad0245df
JamesJi <jiqi@gwu.edu>
2018-06-13 20:49:46 -0700

    start Google

--- medium/280. Wiggle Sort .java
+++ medium/280. Wiggle Sort .java
@@ -1,8 +1,13 @@
 class Solution {
     public void wiggleSort(int[] nums) {
-        for (int i = 1; i < nums.length; i++) {
-            if (((i % 2 == 0) && nums[i] > nums[i - 1]) || ((i % 2 == 1) && nums[i] < nums[i - 1])) {
-                swap(nums, i, i - 1);
+        if (nums == null || nums.length < 2) {
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (i % 2 == 1 && nums[i - 1] > nums[i]) {
+                swap(nums, i - 1, i);
+            } else if (i != 0 && i % 2 == 0 && nums[i - 1] < nums[i]) {
+                swap(nums, i - 1, i);
             }
         }
     }
