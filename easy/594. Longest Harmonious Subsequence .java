9af7aa579c23b183a06e9060c08bc83589609e82
JamesJi <jiqi@gwu.edu>
2018-06-16 21:20:01 -0700

    good night

--- easy/594. Longest Harmonious Subsequence .java
+++ easy/594. Longest Harmonious Subsequence .java
@@ -20,3 +20,26 @@ class Solution {
         return max;
     }
 }
+
+class Solution {
+    public int findLHS(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int left = 0;
+        int right = 0;
+        int max = 0;
+        Arrays.sort(nums);
+        while (right < nums.length) {
+            if (nums[right] - nums[left] < 1) {
+                right++;
+            } else if (nums[right] - nums[left] == 1) {
+                max = Math.max(max, right - left + 1);
+                right++;
+            } else {
+                left++;
+            }
+        }
+        return max;
+    }
+}
