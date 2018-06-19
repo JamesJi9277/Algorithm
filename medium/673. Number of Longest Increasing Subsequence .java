83102f364b5ba1c352c64a7e5977355f916291a0
JamesJi <jiqi@gwu.edu>
2018-06-19 16:02:21 -0700

    continue

--- medium/673. Number of Longest Increasing Subsequence .java
+++ medium/673. Number of Longest Increasing Subsequence .java
@@ -34,3 +34,38 @@ class Solution {
         return globalCount;
     }
 }
+
+class Solution {
+    public int findNumberOfLIS(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int[] len = new int[nums.length];
+        int[] count = new int[nums.length];
+        int lenMax = 0;
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            len[i] = 1;
+            count[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    if (len[i] == len[j] + 1) {
+                        count[i] += count[j];
+                    }
+                    if (len[i] < len[j] + 1) {
+                        len[i] = len[j] + 1;
+                        count[i] = count[j];
+                    }
+                }
+            }
+            if (lenMax == len[i]) {
+                res += count[i];
+            } else if (lenMax < len[i]) {
+                lenMax = len[i];
+                res = count[i];
+            }
+        }
+        return res;
+        
+    }
+}
