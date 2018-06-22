797e96a5ea22a3f35324c1345f751e047a43d6e1
JamesJi <jiqi@gwu.edu>
2018-06-21 21:46:41 -0700

    good night

--- medium/215. Kth Largest Element in an Array .java
+++ medium/215. Kth Largest Element in an Array .java
@@ -65,3 +65,67 @@ class Solution {
         }
     }
 }
+
+
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        if (nums == null || nums.length == 0 || nums.length < k) {
+            return 0;
+        }
+        // Onlogn
+        // Arrays.sort(nums);
+        // return nums[nums.length - k];
+        // On, Onlogn
+        // Comp comp = new Comp();
+        // PriorityQueue<Integer> pq = new PriorityQueue<Integer>(k, comp);
+        // for (int i : nums) {
+        //     if (pq.isEmpty()) {
+        //         pq.offer(i);
+        //     } else if (pq.size() < k) {
+        //         pq.offer(i);
+        //     } else if (pq.size() == k && i > pq.peek()) {
+        //         pq.poll();
+        //         pq.offer(i);
+        //     }
+        // }
+        // return pq.peek();
+        return findKth(nums, 0, nums.length - 1, nums.length - k + 1);
+    }
+    private int findKth(int[] nums, int start, int end, int k) {
+        int left = start;
+        int right = end;
+        int pivot = nums[left];
+        while (left < right) {
+            while (left < right && nums[right] > pivot) {
+                right--;
+            }
+            if (left < right) {
+                nums[left] = nums[right];
+                left++;
+            }
+            while (left < right && nums[left] < pivot) {
+                left++;
+            }
+            if (left < right) {
+                nums[right] = nums[left];
+                right--;
+            }
+        }
+        nums[left] = pivot;
+        if (left + 1 == k) {
+            return pivot;
+        } else if (left + 1 < k) {
+            return findKth(nums, left + 1, end, k);
+        } else {
+            return findKth(nums, start, left - 1, k);
+        }
+    }
+}
+// class Comp implements Comparator<Comp> {
+//     public Comp() {};
+//     @Override
+//     public int compare(Integer i, Integer j) {
+//         return j - i;
+//     }
+// }
