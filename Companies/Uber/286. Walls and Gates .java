396a2b19f3c1664a89b4b97cd96b55b4a608c706
JamesJi <jiqi@gwu.edu>
2018-09-10 20:09:46 -0700

    keep clear

--- Companies/Uber/286. Walls and Gates .java
+++ Companies/Uber/286. Walls and Gates .java
@@ -22,9 +22,8 @@ class Solution {
         helper(rooms, row, col - 1, step + 1);
     }
 }
-
 class Solution {
-    private static final int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    private static final int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
     public void wallsAndGates(int[][] rooms) {
         if (rooms == null || rooms.length == 0 || rooms[0].length == 0) {
             return;
@@ -37,15 +36,17 @@ class Solution {
                 }
             }
         }
+        int step = 0;
         while (!queue.isEmpty()) {
             int size = queue.size();
+            step++;
             for (int i = 0; i < size; i++) {
                 int[] temp = queue.poll();
                 for (int[] dir : directions) {
                     int x = temp[0] + dir[0];
                     int y = temp[1] + dir[1];
-                    if (x >= 0 && x < rooms.length && y >= 0 && y < rooms[0].length && rooms[x][y] != -1 && rooms[x][y] > rooms[temp[0]][temp[1]] + 1) {
-                        rooms[x][y] = rooms[temp[0]][temp[1]] + 1;
+                    if (x >= 0 && x < rooms.length && y >= 0 && y < rooms[0].length && rooms[x][y] != -1 && rooms[x][y] > rooms[temp[0]][temp[1]]) {
+                        rooms[x][y] = step;
                         queue.offer(new int[]{x, y});
                     }
                 }
